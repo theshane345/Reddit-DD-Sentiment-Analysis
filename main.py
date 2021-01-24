b0ed7ddff84afc82fbc49145bf5487b3e04bc537
Shane <shaneor1@hotmail.com>
2021-01-24 16:25:21 +0000

    percentage and ticker input

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import praw
 
 reddit = praw.Reddit(client_id='lELNGnTUoXms4Q', client_secret='vjkYp6AJfkA4lHRaiA_S2KkTQl_ndQ', user_agent='DD Analysis')
 
-subs = ['investing','stocks']
+subs = ['investing','stocks','wallstreetbets']
+tick=input("Please enter ticker symbol (leave blank for all)")
 
 for sub in subs:
-    getHot = reddit.subreddit(sub).new(limit=1000)
+    getHot = reddit.subreddit(sub).new(limit=None)
     lst = []
     for post in getHot:
-        
-        if 'DD' in post.title:
+
+        if 'dd' in post.title.lower() and tick in post.title.lower() and 'reddit' not in post.title.lower()  and '?' not in post.title.lower() :
             text = post.selftext
-            length = len(text)
-            line = str(length) + ' '+ post.title
+            length = round((len(text)/40000)*100)
+            line = post.title + ' -' + str(length) + '%'
             lst.append(line)
             
         
